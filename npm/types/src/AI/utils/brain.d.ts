e9496f34b57e5cffda9f238a47693fdb2b0b1335
Andndre <agungandre687@gmail.com>
2022-07-28 13:27:57 +0800

    'update'

--- npm/types/src/AI/utils/brain.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChessGame } from '../../chessGame.js';
-export declare const getMoveUsingMinMax: (chessGame: ChessGame) => {
-    result: {
-        from: number;
-        to: number;
-    };
-    highScore: number;
-};
--- npm/types/src/AI/utils/minimax.d.ts
+++
@@ -1,2 +0,0 @@
-import { ChessGame } from '../../chessGame.js';
-export declare const minimax: (chessState: ChessGame, depth: number, alpha: number, beta: number, maximizingPlayer?: boolean) => number;
